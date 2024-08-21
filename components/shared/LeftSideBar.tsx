14f13ecf3b1ddcf11acebee5902449537b5f6ca3
castelinos <sheltcas10@gmail.com>
2024-08-21 23:39:02 +0530

    created constants for navbar links & mapped them in UI

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import { navLinks } from '../../lib/constants/index.js'
 
 export default function LeftSidebar(){
     return(
-        <section className="h-screen w-fit">
-            <div className="mt-6 flex flex-col">
-                <div className="flex justify-center items-center gap-4 p-4 mx-8">
-                    <HomeOutlinedIcon className="text-[30px]" />
-                    <Link className="text-[20px] font-light" href="home">Home</Link>
-                </div>
+        <section className="h-screen w-[300px] bg-gray-100">
+            <div className="mt-6 flex flex-col gap-2">
+                {
+                    navLinks.map((link)=>{
+                        return(
+                            <div key={link.name} className="flex justify-start items-center gap-4 mx-6 p-2 text-gray-600 bg-gray-200 hover:text-white rounded-lg hover:bg-green-600">
+                                <span className="text-[30px] ml-4">{ link.icon }</span>
+                                <Link className="text-[18px]" href={link.path}>{link.text}</Link>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </section>
     )
