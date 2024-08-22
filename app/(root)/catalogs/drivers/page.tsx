d093f778e9022a2d9923ec777140ac7cbabc2b89
castelinos <sheltcas10@gmail.com>
2024-08-22 12:24:15 +0530

    Display list of resources in catalog pages

--- app/(root)/catalogs/drivers/page.tsx
+++ app/(root)/catalogs/drivers/page.tsx
@@ -1,17 +1,22 @@
+import DriverCard from "@/components/cards/DriverCard";
 import TitleBar from "@/components/shared/TitleBar";
 import { getDrivers } from "@/lib/actions/driver.actions";
 
 export default async function Drivers() {
-  let data = await getDrivers();
+  let response = await getDrivers();
     
   return (
     <main>
       <TitleBar title="Drivers" />
-      <ul>
+      <section className="flex justify-end py-3 px-6">
+        <input type="submit" value="Create new" className="text-white px-3 py-1 rounded-md bg-green-600"/>
+      </section>
+      <ul className="mx-4 flex flex-col gap-3">
         {
-            data && data.data && data.data.map((driver: any)=>{
+            response && ( response.data.length > 0 ) && response.data.map((driver: any, index:number)=>{
+                const { id, name, contact } = driver;
                 return(
-                    <li key={driver?.id} className="">{driver?.name}</li>
+                  <DriverCard key={index} id={id} name={name} contact={contact} />
                 )
             })
         }
--- app/(root)/catalogs/vehicles/page.tsx
+++ app/(root)/catalogs/vehicles/page.tsx
@@ -1,11 +1,33 @@
+import VehicleCard from "@/components/cards/VehicleCard";
 import TitleBar from "@/components/shared/TitleBar";
-import Image from "next/image";
+import { getVehicles } from "@/lib/actions/vehicle.actions";
 
-export default function Vehicles() {
+export default async function Vehicles() {
+
+  const response = await getVehicles();
+  
   return (
     <main>
       <TitleBar title="Vehicles" />
-      vehicles page
+      <section className="flex justify-end py-3 px-6">
+        <input type="submit" value="Create new" className="text-white px-3 py-1 rounded-md bg-green-600"/>
+      </section>
+
+      <section className="mx-4 grid grid-cols-2 gap-x-2 gap-y-4">
+        {
+            response && ( response.data.length > 0 ) && response.data.map((vehicle: any, index:number)=>{
+                const { vehicleNumber, brandName, vehicleType } = vehicle;
+                return(
+                  <VehicleCard 
+                    key={index} 
+                    vehicleNumber={vehicleNumber} 
+                    brandName={brandName} 
+                    vehicleType={vehicleType} 
+                  />
+                )
+            })
+        }
+      </section>
     </main>
   );
 }
