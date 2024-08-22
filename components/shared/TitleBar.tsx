a6fea9b782e023ae1d48f08f3c7ba8b72175bc8b
castelinos <sheltcas10@gmail.com>
2024-08-22 12:16:39 +0530

    Render subheadings tag only if text is present

--- components/shared/TitleBar.tsx
+++ components/shared/TitleBar.tsx
@@ -7,7 +7,9 @@ export default async function TitleBar( props: TitleBarProps ){
     return(
         <section className="rounded-xl mx-4 px-4 py-4 bg-gray-100">
             <h1 className="text-[32px] righteous font-bold">{ props.title }</h1>
-            <p className="text-[15px] text-gray-500 font-semibold">{ props.subheading }</p>
+            {
+                props.subheading && <p className="text-[15px] text-gray-500 font-semibold">{ props.subheading }</p>
+            }
         </section>
     )
 }
