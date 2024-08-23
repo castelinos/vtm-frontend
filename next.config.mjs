b1a00af08b41919f7411beef1f1f2c4ea13dedfe
castelinos <sheltcas10@gmail.com>
2024-08-23 18:00:58 +0530

    enable settings to allow images from localhost

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   /*  images: {
+        domains: ['localhost']
+    }  Allows images from localhost domains*/
+};
 
 export default nextConfig;
